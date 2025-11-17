ff3aefaac4808fa3291ee74228f2d55f22b437fe
rahimwws <umudyan2014@gmail.com>
2025-11-17 09:15:54 +0800

    fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,15 +7,43 @@ export default function Hero() {
   const heroText = useMemo(
     () => (
       <>
-        <TextAnimate
-          className="text-4xl md:text-6xl lg:text-8xl font-regular leading-tight"
-          style={{ color: "#161617", letterSpacing: "-0.04em" }}
-          animation="blurInDown"
-          by="character"
-          once
-        >
-          less talk. more launch.
-        </TextAnimate>
+        <div className="flex items-center gap-2 justify-center">
+          <TextAnimate
+            className="text-4xl md:text-6xl lg:text-8xl font-regular leading-tight"
+            style={{ color: "#161617", letterSpacing: "-0.04em" }}
+            animation="blurInDown"
+            by="character"
+            once
+          >
+            0
+          </TextAnimate>
+          <svg
+            width="18"
+            height="16"
+            viewBox="0 0 18 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 h-5 md:w-15 md:h-15  transition-transform duration-300"
+          >
+            <path
+              d="M3 8H15M15 8L10 3M15 8L10 13"
+              className="group-hover:!stroke-white transition-colors duration-300"
+              stroke="black"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <TextAnimate
+            className="text-4xl md:text-6xl lg:text-8xl font-regular leading-tight"
+            style={{ color: "#161617", letterSpacing: "-0.04em" }}
+            animation="blurInDown"
+            by="character"
+            once
+          >
+            1
+          </TextAnimate>
+        </div>
 
         <TextAnimate
           className="text-4xl md:text-6xl lg:text-8xl font-light leading-tight mb-4 mt-[-10px] md:mt-[-20px]"
@@ -25,7 +53,7 @@ export default function Hero() {
           once
           delay={0.3}
         >
-          mobile · web3 · ai
+          Software Partner
         </TextAnimate>
       </>
     ),
@@ -42,13 +70,13 @@ export default function Hero() {
             className="text-base md:text-xl font-normal"
             style={{ color: "#161617" }}
           >
-            we build mobile apps for web3 and AI products
+            we build apps for web3 and ai startups
           </p>
           <p
             className="text-base md:text-xl font-medium"
             style={{ color: "#C9CDD2" }}
           >
-            with taste - from prototype to release.
+            with taste - from idea to release.
           </p>
         </div>
       </div>
